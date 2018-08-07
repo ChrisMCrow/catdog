0fa3e833b926058f15af33a95b09db2f35794cc0
catherine bradley and Chris Crow <catherine@>
2018-08-07 15:44:38 -0700

    add remove click on first child

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
   $("button#hello").click(function() {
-    $("ul").prepend("<li>Hello!</li>");
+    $("ul#user").prepend("<li>Hello!</li>");
+    $("ul#webpage").prepend("<li>Why hello there!</li>")
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
 
   $("button#goodbye").click(function() {
